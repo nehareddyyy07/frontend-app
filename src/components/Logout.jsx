7ddb05a9dbbed6f58e13b8fd4a3bbcb5b324579b
nehareddyyy07 <nehareddypeduthala@gmail.com>
2026-03-13 12:03:05 +0530

    new changes

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,13 @@
-import {Link} from "react-router-dom";
-function Logout(){
-    return(
-        <h2>Logout Page</h2>
-    )
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import App, { AppContext } from "../App";
+function Logout() {
+  const { user, setUser } = useContext(AppContext);
+  const Navigate = useNavigate();
+  useEffect(() => {
+    setUser({});
+    Navigate("/");
+  }, []);
+  return <h2>Logout Page</h2>;
 }
 export default Logout;
